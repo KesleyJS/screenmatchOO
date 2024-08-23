bfe4bb53e7685494e9270dff500526cc784154a8
KesleyJDS <kesley.jorge@tekeezy.com.br>
2024-08-23 18:57:34 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import br.com.alura.screenmatch.models.Movie;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setName("The Hangover");
+        movie.setReleaseYear(2010);
+        movie.setDurationMinutes(96);
+
+        movie.rate(10);
+        movie.rate(3);
+        movie.rate(1);
+        movie.showInfo();
+        System.out.println(movie.getAverage());
+        movie.showInfo();
+
+
+    }
+}
